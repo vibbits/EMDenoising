f33342500a25d729748e58a835297ca38f37573b
Frank Vernaillen <frank.vernaillen@ugent.be>
2016-11-25 10:20:51 +0100

    Change ImagePanel API so no image (image == null) is also allowed.

--- DenoisingIJ2/src/main/java/be/vib/imagej/ImagePanel.java
+++ DenoisingIJ2/src/main/java/be/vib/imagej/ImagePanel.java
@@ -1,36 +1,38 @@
 package be.vib.imagej;
 
-import java.awt.Graphics;
-import java.awt.Paint;
-import java.awt.image.BufferedImage;
-
-import javax.swing.BorderFactory;
-import javax.swing.JPanel;
-
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class ImagePanel extends JPanel
 {
 	private BufferedImage image;
-	private int maxSize;
+	private final JPanel parent;
+	private String text; // Info text shown on top of the image (if non-null, if null no text is shown).
 	
-	public ImagePanel()
+	public ImagePanel(JPanel parent)
 	{
+		this.parent = parent;
+		this.text = null;
+		this.image = null;
 	}
 	
-	public ImagePanel(BufferedImage image, int maxSize)
-	{		
-		setImage(image, maxSize);
+	// For layout debugging:
+	// this.setBorder(BorderFactory.createDashedBorder(null));
+	
+	// Interesting: Deven_C_Miller's answer in
+	// http://stackoverflow.com/questions/2155351/swing-jpanel-wont-repaint
+	
+	public void setImage(BufferedImage image)
+	{
+		System.out.println("ImagePanel.setImage " + image + " EDT? " + SwingUtilities.isEventDispatchThread());
 		
-		// For layout debugging:
-		// this.setBorder(BorderFactory.createDashedBorder(null));
-	}
-	
-	public void setImage(BufferedImage image, int maxSize)
-	{
 		this.image = image;
-		this.maxSize = maxSize;
 		
 		// If the image size has changed, it also implicitly modified the
 		// minimum/preferred/maximum sizes, so we must notify the layout manager.
@@ -38,68 +40,56 @@ public class ImagePanel extends JPanel
 		
 		// The image's content may have changed, so redraw the image.
 		repaint();
-	}
-
-	private Dimension bestSize()
-	{
-		if (image == null)
-			return new Dimension();
 		
-		float scale = getScale();
-		int w = (int)(image.getWidth() * scale);
-		int h = (int)(image.getHeight() * scale);
-		return new Dimension(w, h);
+		// Trigger a redraw of the parent panel of ImagePanel to redraw itself (and all its descendants).
+		parent.validate();
 	}
 	
-	@Override
-	public Dimension getPreferredSize()
+	public void setText(String text)
 	{
-		return bestSize();
+		System.out.println("ImagePanel.setText " + " EDT? " + SwingUtilities.isEventDispatchThread());
+		this.text = text;
+		repaint();
 	}
 	
 	@Override
 	public Dimension getMinimumSize()
 	{
-		return bestSize();
+		return getPreferredSize();
 	}
 	
 	@Override
 	public Dimension getMaximumSize()
 	{
-		return bestSize();
-	}
-	
-	public float getScale()
-	{
-		if (image == null)
-			return 1.0f;
-		
-		int width = image.getWidth();
-		int height = image.getHeight();
-		
-		int actualSize = Math.max(width, height);
-		
-		if (actualSize <= maxSize)
-		{
-			return 1.0f;
-		}
-		else
-		{
-			return (float)maxSize / (float)actualSize;		
-		}	
+		return getPreferredSize();
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g)
 	{
+		//System.out.println("ImagePanel.paintComponent " + image + " EDT? " + SwingUtilities.isEventDispatchThread());
+
 		super.paintComponent(g);
-		if (image == null)
-			return;
 		
-		Dimension dim = bestSize();		
-		// System.out.println("ImagePanel size: " + getSize() + " bestSize: " + bestSize() + " scale: " + getScale() + "image size: " + image.getWidth() + " x " + image.getHeight());
+		Dimension dim = getPreferredSize();		
+		//System.out.print("ImagePanel preferred size: " + dim + " Image:" + (image == null ? "null" : image));
 		
-		g.drawImage(image, 0, 0, dim.width, dim.height, Color.WHITE, null);  // left pixels of image are at x=0, right pixels at x=dim.width - 1 (so not at x=dim.width)
-		g.drawRect(0, 0, dim.width - 1, dim.height - 1);                     // left rectangle edge is at x=0, right edge at x=dim.width - 1
+		if (image != null)
+		{
+			g.drawImage(image, 0, 0, dim.width, dim.height, Color.WHITE, null);  // left pixels of image are at x=0, right pixels at x=dim.width - 1 (so not at x=dim.width)
+			g.drawRect(0, 0, dim.width - 1, dim.height - 1);                     // left rectangle edge is at x=0, right edge at x=dim.width - 1
+		}
+
+//      FIXME: If the text is larger than the preferred size of the window,
+//             it will not be rendered nicely but will be clipped...
+//	
+//		if (text != null)
+//		{
+//			System.out.println("drawing text: '" + text + "' at " + 0 +" " + dim.height / 2);
+//			FontMetrics metrics = g.getFontMetrics();
+//			int x = (dim.width - metrics.stringWidth(text)) / 2;
+//			int y = metrics.getHeight() + 10;
+//			g.drawChars(text.toCharArray(), 0, text.length(), x, y);
+//		}
 	}
 }
