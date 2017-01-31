94d52481bf7235f36e3343c22cdd1c66b5ec9368
Frank Vernaillen <frank.vernaillen@ugent.be>
2017-01-31 22:44:08 +0100

    Document possible configuration options in script for starting Fiji.

--- run.bat
+++ run.bat
@@ -1,1 +1,4 @@
+@rem e:\Fiji.app\ImageJ-win64.exe --console -Dvib.emrestoration.quasar.engine=cpu -Dvib.emrestoration.quasar.resources=E:\git\bits\bioimaging\EMDenoising\src\main\resources\quasar
+@rem (the engine can be any engine that Quasar supports, for example cuda, cpu, ...)
+
 e:\Fiji.app\ImageJ-win64.exe --console
