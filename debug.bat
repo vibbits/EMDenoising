597b000a77adab4d5d55008ae8478d97b3c79f31
Frank Vernaillen <frank.vernaillen@ugent.be>
2017-05-02 15:41:27 +0200

    Add script for Java debugging of the denoising plugin.

---
+++ debug.bat
@@ -0,0 +1,2 @@
+echo Make sure Eclipse is using the Remote Java Application debug configuration, with connection type Standard (Socket Attach), host=localhost, port=8888
+e:\Fiji.app\ImageJ-win64.exe --console --debugger=8888
