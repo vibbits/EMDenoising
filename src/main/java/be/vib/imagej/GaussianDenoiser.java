34d5d0e781fbe61eb29a1e98f4fdfea706a83469
Frank Vernaillen <frank.vernaillen@ugent.be>
2017-05-11 14:31:04 +0200

    Reduce image range to [0,1] before applying the Gaussian filter.
    Right now it is just an experiment and not really needed yet. But we may
    need to do it if we want to use denoising parameters that are a function
    of the estimated noise level.

--- src/main/java/be/vib/imagej/GaussianDenoiser.java
+++ src/main/java/be/vib/imagej/GaussianDenoiser.java
@@ -3,6 +3,7 @@ package be.vib.imagej;
 import java.nio.file.NoSuchFileException;
 
 import be.vib.bits.QFunction;
+import be.vib.bits.QUtils;
 import be.vib.bits.QValue;
 import ij.process.ImageProcessor;
 
@@ -18,17 +19,23 @@ public class GaussianDenoiser extends Denoiser
 	
 	@Override
 	public ImageProcessor call() throws NoSuchFileException
-	{
+	{		
 		QFunction gaussian = new QFunction("gaussian_filter(mat,scalar,int,string)");
 		
 		QValue noisyImageCube = QuasarTools.newCubeFromImage(image);
 		
+		float r = QuasarTools.bitRange(image);
+		
+		QUtils.inplaceDivide(noisyImageCube, r);  // scale pixels values from [0, 255] or [0, 65535] down to [0, 1]
+
 		QValue denoisedImageCube = gaussian.apply(noisyImageCube,
 							                      new QValue(params.sigma),
 							                      new QValue(0),
 							                      new QValue("mirror"));
 		
 		noisyImageCube.dispose();
+
+		QUtils.inplaceMultiply(denoisedImageCube, r); // scale pixels values back to [0, 255] or [0, 65535]
 
 		ImageProcessor denoisedImage = QuasarTools.newImageFromCube(image, denoisedImageCube);
 
--- src/main/java/be/vib/imagej/GaussianParams.java
+++ src/main/java/be/vib/imagej/GaussianParams.java
@@ -5,7 +5,7 @@ public class GaussianParams
 	public float sigma;
 	
 	public static final float sigmaMin = 0.5f;
-	public static final float sigmaMax = 10.0f;
+	public static final float sigmaMax = 5.0f;
 	
 	public GaussianParams()
 	{
@@ -34,5 +34,11 @@ public class GaussianParams
 	public int hashCode()
 	{
 		return Float.valueOf(sigma).hashCode();
+	}
+
+	// Set default algorithm parameters based on the image noise estimate 'sigmaEstimate'.
+	public void setDefaultParameters(float sigmaEstimate)
+	{
+		sigma = 8.06167602539063f * sigmaEstimate + 0.534878730773926f;
 	}
 }
