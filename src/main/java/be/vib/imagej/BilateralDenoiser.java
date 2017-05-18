d864f9905e951e617f1359b7a55e3eccbbb3a1ec
Frank Vernaillen <frank.vernaillen@ugent.be>
2017-05-18 16:00:32 +0200

    Workaround for denoising 16-bit images with the bilateral filter.

--- src/main/java/be/vib/imagej/BilateralDenoiser.java
+++ src/main/java/be/vib/imagej/BilateralDenoiser.java
@@ -3,6 +3,7 @@ package be.vib.imagej;
 import java.nio.file.NoSuchFileException;
 
 import be.vib.bits.QFunction;
+import be.vib.bits.QUtils;
 import be.vib.bits.QValue;
 import ij.process.ImageProcessor;
 
@@ -19,13 +20,22 @@ public class BilateralDenoiser extends Denoiser
 	@Override
 	public ImageProcessor call() throws NoSuchFileException
 	{
-		QFunction bilateralFilter = new QFunction("bilateral_filter_denoise(cube,cube,int,scalar)");  // Fast histogram based O(1) bilateral filter
-		// FIXME: the O(1) implementation does not currently support 16 bit/pixel images.
+		QFunction bilateralFilter = new QFunction("bilateral_filter_denoise(cube,cube,int,scalar)");  // Fast histogram based bilateral filter
 		
 		QFunction zeros = new QFunction("zeros(...)");
 		
 		QValue noisyImageCube = QuasarTools.newCubeFromImage(image);
 		
+		float r = QuasarTools.bitRange(image);
+		
+		// The histogram based bilateral filter code in Quasar expects pixel values in the [0, 255] range.
+		// If we have a 16-bit image, we scale the values down to 8-bit for now - this probably loses precision.
+		// TODO: natively support 16 bit images the Quasar bilateral filter implementation
+		if (r != 255)
+		{
+			QUtils.inplaceMultiply(noisyImageCube, 255.0f / r);
+		}
+
 		// Construct an empty result image. It will be filled in by bilateralFilter().
 		QValue denoisedImageCube = zeros.apply(noisyImageCube.size());
 		
@@ -35,6 +45,11 @@ public class BilateralDenoiser extends Denoiser
 				              new QValue(-params.h));  // params.h is actually the negative of h (to avoid negative values in the user interface)
 		
 		noisyImageCube.dispose();
+		
+		if (r != 255)
+		{
+			QUtils.inplaceMultiply(denoisedImageCube, r / 255.0f);
+		}
 
 		ImageProcessor denoisedImage = QuasarTools.newImageFromCube(image, denoisedImageCube);
 
