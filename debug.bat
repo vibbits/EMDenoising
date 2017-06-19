847c5f9e080096d3cd4047d98cbeb01a8e31219d
Frank Vernaillen <frank.vernaillen@ugent.be>
2017-06-19 13:18:02 +0200

    Improved comment a bit.

--- debug.bat
+++ debug.bat
@@ -1,2 +1,4 @@
-echo Make sure Eclipse is using the Remote Java Application debug configuration, with connection type Standard (Socket Attach), host=localhost, port=8888
+@rem To debug the plugin, first start Fiji using this .bat file. Then, in Eclipse, attach the debugger.
+@rem Make sure Eclipse is using the Remote Java Application debug configuration,
+@rem with connection type Standard (Socket Attach), host=localhost, port=8888
 e:\Fiji.app\ImageJ-win64.exe --console --debugger=8888
