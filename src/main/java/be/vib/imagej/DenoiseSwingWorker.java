a9793e60f6560b39b8fbee9ca3a490affdd26358
Frank Vernaillen <frank.vernaillen@ugent.be>
2017-06-28 11:29:58 +0200

    Add slice range to title of denoised image.

--- src/main/java/be/vib/imagej/DenoiseSwingWorker.java
+++ src/main/java/be/vib/imagej/DenoiseSwingWorker.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutionException;
 import javax.swing.JProgressBar;
 import javax.swing.SwingWorker;
 
+import be.vib.imagej.ImageRange.RangeType;
 import ij.ImagePlus;
 
 // The DenoiseSwingWorker class is a wrapper around the DenoiseEngine class.
@@ -45,6 +46,35 @@ public class DenoiseSwingWorker extends SwingWorker<ImagePlus, Integer>
 		}
 	}
 	
+	// Return a title for the denoised image. It includes the title of the noisy input image,
+	// the denoising algorithm, and (if relevant) the range of slices that were denoised.
+	private String getTitle()
+	{
+		String rangeString = "";
+		
+		if (noisyImagePlus.getNSlices() > 1)
+		{
+			if (range.getType() == RangeType.CURRENT_SLICE)
+			{
+				rangeString = " slice " + range.getFirst();
+			}
+			else if (range.getType() == RangeType.NUMERIC_SLICE_RANGE)
+			{
+				if (range.getFirst() == range.getLast())
+				{
+					rangeString = " slice " + range.getFirst();
+				}
+				else if (range.getFirst() != 1 || range.getLast() != noisyImagePlus.getNSlices())
+				{
+					rangeString = " slices " + range.getFirst() + "-" + range.getLast();
+				}
+			}
+		}
+		
+		String title = noisyImagePlus.getTitle() + rangeString + " ["+ algorithm.getReadableName() + "]";
+		return title;
+	}
+	
 	public DenoiseSwingWorker(Algorithm algorithm, ImagePlus noisyImagePlus, ImageRange range, JProgressBar progressBar, Runnable whenDone)
 	{
 		this.algorithm = algorithm;
@@ -61,9 +91,7 @@ public class DenoiseSwingWorker extends SwingWorker<ImagePlus, Integer>
 		// Do not update Java Swing components here.
 		
 		DenoiseEngine engine = new SwingDenoiseEngine(algorithm);
-
-		String title = noisyImagePlus.getTitle() + " ["+ algorithm.getReadableName() + "]";
-		return engine.denoise(noisyImagePlus, range, title);
+		return engine.denoise(noisyImagePlus, range, getTitle());
 	}
 	
 	@Override
