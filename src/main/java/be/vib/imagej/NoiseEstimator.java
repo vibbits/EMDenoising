b89d9c952976b6bd84ea9baa968efc525e1cd35b
Frank Vernaillen <frank.vernaillen@ugent.be>
2018-04-04 17:10:54 +0200

    Estimate noise from larger portion of the image in case we're using MAD.

--- src/main/java/be/vib/imagej/NoiseEstimator.java
+++ src/main/java/be/vib/imagej/NoiseEstimator.java
@@ -12,11 +12,18 @@ public class NoiseEstimator implements Callable<Float>
 {
 	private ImageProcessor image;
 	
+	private static boolean liuNoiseEstimation = false; // if true then the Liu noise estimator is used; otherwise MAD is used
+	// Note: Currently there are several issues with the Liu noise implementation in Quasar
+	// (tau0 hard-coded, negative eigenvalues for covariance matrix,
+	// excessive memory usage even for images of reasonable size - 1500 x 1500 pixels)
+	// so we use MAD instead.
+	
 	NoiseEstimator(ImageProcessor image)
 	{		
-		// The implementation of the Liu noise estimator in estimate_noise.q allocates quite large memory blocks: on the order of ((7 x 7) x (width x height) x sizeof(float))
-		// To avoid CUDA out-of-memory conditions on larger images we estimate the noise from the central 512x512 pixel region of the image.
-		this.image = restrictSize(image, 512);
+		// Restrict the noise estimation to the central region of the image (for the sake of performance but especially of memory efficiency).
+		// The current implementation of the Liu noise estimator in estimate_noise.q allocates quite large memory blocks:
+		// on the order of ((7 x 7) x (width x height) x sizeof(float)). 
+		this.image = liuNoiseEstimation ? restrictSize(image, 512) : restrictSize(image, 2048);
 	}
 	
 	private ImageProcessor restrictSize(ImageProcessor image, int maxSize)
@@ -27,7 +34,7 @@ public class NoiseEstimator implements Callable<Float>
 		int y = (image.getHeight() - h) / 2;
 		Rectangle rect = new Rectangle(x, y, w, h);
 				
-		ImageProcessor imageCopy = image.duplicate();  // CHECKME: we copy the image processor to avoid changing its ROI, is this really needed?
+		ImageProcessor imageCopy = image.duplicate();  // copy the image processor to avoid changing its ROI
 		imageCopy.setRoi(rect);
 		return imageCopy.crop();		
 	}
@@ -37,17 +44,17 @@ public class NoiseEstimator implements Callable<Float>
 	{
 		QValue noisyImageCube = ImageUtils.newCubeFromImage(image);
 		
-//		QFunction imwrite = new QFunction("imwrite(string,cube)");
-//		imwrite.apply(new QValue("e:\\noisy.tif"), noisyImageCube);
-
+		// For testing
+		// QFunction imwrite = new QFunction("imwrite(string,cube)");
+		// imwrite.apply(new QValue("e:\\noisy.tif"), noisyImageCube);
+		
 		float r = ImageUtils.bitRange(image);
 		
 		QUtils.inplaceDivide(noisyImageCube, r);  // scale pixels values from [0, 255] or [0, 65535] down to [0, 1]
-		
+				
 		QValue noise = null;
-		
-		boolean liu = false; // There are several issues with this implementation (tau0 hard-coded, negative eigenvalues for covariance matrix, excessive memory usage even for images of reasonable size - 1500 x 1500 pixels) 
-		if (liu)
+				 
+		if (liuNoiseEstimation)
 		{
 			QFunction estimateNoise = new QFunction("V_estimate_noise(mat)"); 
 			// Note: the V_ is a workaround for a problem in the current version of Quasar when the function name (estimate_noise) is identical to the source file name (estimate_noise.q)
@@ -60,7 +67,7 @@ public class NoiseEstimator implements Callable<Float>
 		}
 		
 		noisyImageCube.dispose();
-
+		
 		return noise.getFloat();		
 	}
 
