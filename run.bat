7976793bd2ae7ad9170c68c4da11bafcd956ab25
Frank Vernaillen <frank.vernaillen@ugent.be>
2019-02-21 16:24:41 +0100

    Removed obsolete comment in batch file.

--- run.bat
+++ run.bat
@@ -1,4 +1,1 @@
-@rem e:\Fiji.app\ImageJ-win64.exe --console -Dvib.emrestoration.quasar.engine=cpu -Dvib.emrestoration.quasar.resources=E:\git\bits\bioimaging\EMDenoising\src\main\resources\quasar
-@rem (the engine can be any engine that Quasar supports, for example cuda, cpu, ...)
-
-e:\Fiji.app\ImageJ-win64.exe --console
+E:\Fiji.app\ImageJ-win64.exe --console
