ae492a8a0cafb34e26f664f151a8379d3412a704
Frank Vernaillen <frank.vernaillen@ugent.be>
2019-02-21 16:23:21 +0100

    Bumped up version numbers.

--- src/main/java/be/vib/imagej/AboutDialog.java
+++ src/main/java/be/vib/imagej/AboutDialog.java
@@ -54,7 +54,7 @@ public class AboutDialog implements Command
 			// For example https://stackoverflow.com/questions/30655246/html-text-in-jlabel-ignores-alignment-with-text-align-center
 			
 			JLabel title = new JLabel("<html><div align='center'>" + 
-			                          "<font size=+1>DenoisEM v0.0.1</font><br>" +   // TODO: centralize the version number, or extract it from the jar filename or so
+			                          "<font size=+1>DenoisEM v1.0.1</font><br>" +   // TODO: centralize the version number, or extract it from the jar filename or so
 			                          "</html>", JLabel.CENTER);
 						
 			JLabel subTitle = new JLabel("<html><div align='center'>" + 
@@ -62,7 +62,7 @@ public class AboutDialog implements Command
                                          "</html>", JLabel.CENTER);
 			
 			JLabel copyright = new JLabel("<html><div align='center'>" + 
-                                          "&copy; VIB 2017-2018. All rights reserved." + 
+                                          "&copy; VIB 2017-2019. All rights reserved." + 
                                           "</html>", JLabel.CENTER);
 			
 			JPanel logos = new LogosPanel();
