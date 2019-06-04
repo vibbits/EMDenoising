9180a37463cea6440bac9b2f4abcaa698960726c
Frank Vernaillen <frank.vernaillen@ugent.be>
2019-06-04 16:07:33 +0200

    Removed obsolete installation and build instructions.

--- src/main/java/be/vib/imagej/DenoisingIJ2.java
+++ src/main/java/be/vib/imagej/DenoisingIJ2.java
@@ -5,26 +5,6 @@ import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-// Windows 10
-// ----------
-// Installation:
-//    copy JavaQuasarBridge.jar to e:\Fiji.app\plugins
-//    copy DenoisEM-0.0.1.jar to e:\Fiji.app\plugins
-//
-// Prerequisites:
-//    Fiji
-//    Quasar (with a license, for now - we will eventually embed the Quasar runtime in the JavaQuasarBridge jar)
-//
-// Running:
-//    e:\Fiji.app\ImageJ-win64.exe
-// (or "e:\Fiji.app\ImageJ-win64.exe --console" to see output in a text console)
-//
-// Aside: to compile a .q to .qlib
-//    "e:\Program Files\Quasar\Quasar.exe" --make_lib (--optimize) --gpu nlmeans_denoising_stillimages.q
-//
-// TODO: update comment
-
-
 @Plugin(type = Command.class, menuPath = "Plugins>DenoisEM>Denoise")
 public class DenoisingIJ2 implements Command
 {
@@ -36,19 +16,6 @@ public class DenoisingIJ2 implements Command
 	{
 		log.info("VIB DenoisEM plugin");
 		
-		// TODO
-
-		// A little experiment with the macro recorder 
-		// See e.g. http://imagej.net/PlugIn_Design_Guidelines
-//		if (Recorder.record)
-//		{
-//			Recorder.recordString("// Start the machinery for GPU-acceleration\n");			
-//			Recorder.recordString("call('be.vib.imagej.DenoisingIJ2.startQuasar', 'cuda');\n");
-//			
-//			Recorder.recordString("// Test\n");			
-//			Recorder.recordString("call('be.vib.imagej.DenoisingIJ2.testMethod2', '2', '3');\n");
-//		}
-		
 		Wizard wizard = DenoisingWizardSingleton.getInstance();
 		wizard.setVisible(true);
 		
@@ -56,26 +23,5 @@ public class DenoisingIJ2 implements Command
 		// but the wizard is still visible and active.
 	}
 	
-//	public static String startQuasar(String engine)
-//	{
-//		System.out.println("startQuasar");
-//		try
-//		{
-//			QuasarTools.startQuasar(engine, false);
-//			System.out.println("  startQuasar OK");
-//			return "1";
-//		}
-//		catch (InterruptedException | ExecutionException e)
-//		{
-//			System.out.println("  startQuasar exception");
-//			return "0";
-//		}
-//	}
-//
-//	public static String testMethod2(String arg1, String arg2)
-//	{
-//		System.out.println("testMethod2 received argument arg1=" + arg1 + " arg2="+ arg2 + "and will return the string 123.");
-//		return "123";
-//	}
 }
 	
