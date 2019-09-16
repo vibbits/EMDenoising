889756175c2f2d8d91d7dcf449bd6357395593c0
Frank Vernaillen <frank.vernaillen@ugent.be>
2019-09-16 12:39:53 +0200

    The estimated noise standard deviation is now for images normalized to [0,1] pixel intensities. This is what our default denoising parameter prediction expects, and is also more consistent since both 8 and 16-bit images may get used and mixed.

--- src/main/java/be/vib/imagej/NoiseEstimator.java
+++ src/main/java/be/vib/imagej/NoiseEstimator.java
@@ -8,26 +8,20 @@ import be.vib.bits.QUtils;
 import be.vib.bits.QValue;
 import ij.process.ImageProcessor;
 
+// A Java wrapper around a MAD (median absolute deviation) noise estimator implemented in Quasar.
 public class NoiseEstimator implements Callable<Float>
 {
 	private ImageProcessor image;
 	
-	private static boolean liuNoiseEstimation = false; // if true then the Liu noise estimator is used; otherwise MAD is used
-	// Note: Currently there are several issues with the Liu noise implementation in Quasar
-	// (tau0 hard-coded, negative eigenvalues for covariance matrix,
-	// excessive memory usage even for images of reasonable size - 1500 x 1500 pixels)
-	// so we use MAD instead.
-	
 	NoiseEstimator(ImageProcessor image)
 	{		
-		// Restrict the noise estimation to the central region of the image (for the sake of performance but especially of memory efficiency).
-		// The current implementation of the Liu noise estimator in estimate_noise.q allocates quite large memory blocks:
-		// on the order of ((7 x 7) x (width x height) x sizeof(float)). 
-		this.image = liuNoiseEstimation ? restrictSize(image, 512) : restrictSize(image, 2048);
+		// Restrict the noise estimation to the central region of the image
+		// (for the sake of performance and to limit GPU memory usage in case of huge images).
+		this.image = restrictSize(image, 2048);
 	}
 	
 	// Return a copy of the image, restricted to maxSize x maxSize pixels.
-	// If the image is larger than maxSize, the portion of maxSize pixels centered copy around the middle of the image is returned.
+	// If the image is larger than maxSize, the portion of maxSize pixels centered around the middle of the image is returned.
 	private ImageProcessor restrictSize(ImageProcessor image, int maxSize)
 	{
 		int w = Math.min(maxSize, image.getWidth());
@@ -48,30 +42,22 @@ public class NoiseEstimator implements Callable<Float>
 		
 		// For testing
 		// QFunction imwrite = new QFunction("imwrite(string,cube)");
-		// imwrite.apply(new QValue("e:\\noisy.tif"), noisyImageCube);
+		// imwrite.apply(new QValue("c:\\noisy.tif"), noisyImageCube);	
 		
-		float r = ImageUtils.bitRange(image);
-		
-		QUtils.inplaceDivide(noisyImageCube, r);  // scale pixels values from [0, 255] or [0, 65535] down to [0, 1]
-		
-		QValue noise = null;
-				 
-		if (liuNoiseEstimation)
+		final int dynamicRange = (int)ImageUtils.bitRange(image);
+		if (dynamicRange != 255)
 		{
-			QFunction estimateNoise = new QFunction("V_estimate_noise(mat)"); 
-			// Note: the V_ prefix was a workaround for a problem in a previous version of Quasar when the function name (estimate_noise)
-			// is identical to the source file name (estimate_noise.q). It can probably be removed again.
-			noise = estimateNoise.apply(noisyImageCube);
-		}
-		else
-		{
-			QFunction estimateNoise = new QFunction("estimate_noise_mad(mat,int)");			
-			noise = estimateNoise.apply(noisyImageCube, new QValue(2));
-		}
+			// Scale pixels values to [0, 255] - the Quasar MAD noise estimator expects this.
+			final float r = dynamicRange / 255.0f;
+			QUtils.inplaceDivide(noisyImageCube, r);  
+		}	 
+
+		QFunction estimateNoise = new QFunction("estimate_noise_mad(mat,int)");			
+		QValue noise = estimateNoise.apply(noisyImageCube, new QValue(2));
 		
 		noisyImageCube.dispose();
 		
-		return noise.getFloat();		
+		return noise.getFloat() / 255.0f;  // Return the noise standard deviation, for image pixel intensities normalized to be in [0,1]	
 	}
 
 }
